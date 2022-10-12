113a794ccbcb7d5b990e19087a0c7090b3692891
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-13 00:52:10 +0530

    Add files via upload

---
+++ UC4_Refactor1/Genrics_Find_Maximum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genrics_Find_Maximum
+{
+    public class main
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Maximum Value Of Integer");
+            Console.WriteLine(Refactor.getmax<int>(50, 20, 70)); 
+            Console.WriteLine("Maximum Value Of Flot:");
+            Console.WriteLine(Refactor.getmax<float>(15.3f, 45.5f, 13.2f)); 
+            Console.WriteLine("Maximum Value Of String:");
+            Console.WriteLine(Refactor.getmax<string>("BBB", "CCC", "AAA")); 
+            Console.ReadLine();
+        }
+    }
+    internal class Refactor
+    {
+        public static E getmax<E> (E firstValue, E secondValue, E thirdValue) where E : IComparable
+        {
+              E max = firstValue;
+            if (secondValue.CompareTo(max) > 0)
+                max = secondValue;
+            if (thirdValue.CompareTo(max) > 0)
+                max = thirdValue;
+            return max;
+        }
+    }
+}
